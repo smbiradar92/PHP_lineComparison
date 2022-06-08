3247598a1b3bf14174d6b0e729bc663463136661
Smbiradar92 <smbiradar92@gmail.com>
2022-06-09 00:22:22 +0530

    adding program to compare length of two lines

--- LineComparison.php
+++ LineComparison.php
@@ -24,13 +24,28 @@ class LineComparison{
     function calculateLength(){
         $this->readValues();
         $lengthOfLine = sqrt(pow(($this->x2 - $this->x1),2) + pow(($this->y2 - $this->y1),2));
-        echo "The length of the line between points ($this->x2,$this->x1) and ($this->y2, $this->y1) is :".round($lengthOfLine,0);
+        echo "The length of the line between points ($this->x2,$this->x1) and ($this->y2, $this->y1) is : ".round($lengthOfLine,0);
+    }
+
+    //function to compare line length
+    function compareLength($line1, $line2){
+        if($line1 == $line2){
+            echo "the length of line1 == length of line2\n";
+        }else{
+            echo "length of line1 is not equal to length of line2";
+        }
     }
 
 }
 // function calling
 $line = new LineComparison();
 $line->welcomeMsg();
-$line->calculateLength()
+echo "-----input values for line 1\n";
+$line1= $line->calculateLength();           //calling length calculation function for different lines
+echo "\n------------Result-------------\n";
+echo "-----input values for line 2\n";
+$line2= $line->calculateLength();
+echo "\n";
+$line->compareLength($line1,$line2);
 
 ?>
