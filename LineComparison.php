e208d20074119a3dc8f829656aaabc1985078513
Smbiradar92 <smbiradar92@gmail.com>
2022-06-09 09:09:03 +0530

    program to compare length of line greater, less than or equal to

--- LineComparison.php
+++ LineComparison.php
@@ -22,17 +22,19 @@ class LineComparison{
 
     //function to calculate length
     function calculateLength(){
-        $this->readValues();
         $lengthOfLine = sqrt(pow(($this->x2 - $this->x1),2) + pow(($this->y2 - $this->y1),2));
-        echo "The length of the line between points ($this->x2,$this->x1) and ($this->y2, $this->y1) is : ".round($lengthOfLine,0);
+        //echo "The length of the line between points ($this->x2,$this->x1) and ($this->y2, $this->y1) is : ".round($lengthOfLine,0);
+        return $lengthOfLine;
     }
 
     //function to compare line length
-    function compareLength($line1, $line2){
+    public function compareLength($line1, $line2){
         if($line1 == $line2){
             echo "the length of line1 == length of line2\n";
+        }elseif($line1 > $line2){
+            echo "the length of line1 is greater than length of line2\n";
         }else{
-            echo "length of line1 is not equal to length of line2";
+            echo "length of line2 is greater than length of line2";
         }
     }
 
@@ -40,11 +42,15 @@ class LineComparison{
 // function calling
 $line = new LineComparison();
 $line->welcomeMsg();
+$line1=$line->readValues();
 echo "-----input values for line 1\n";
 $line1= $line->calculateLength();           //calling length calculation function for different lines
+echo "length of line1 is ".round($line1,2);
 echo "\n------------Result-------------\n";
 echo "-----input values for line 2\n";
+$line2=$line->readValues();
 $line2= $line->calculateLength();
+echo "length of line2 is ".round($line2,2);
 echo "\n";
 $line->compareLength($line1,$line2);
 
